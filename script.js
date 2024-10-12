ffe940a6ff6e55830b5126bff5296059d10df006
Kazuma <caspergaming700@gmail.com>
2024-10-12 14:01:13 +0800

    new update dark mode

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ menuIcon.addEventListener('click', () => {
     navList.classList.toggle('active');
 });
 
+let darkMode = document.getElementById("darkMode");
+
+darkMode.onclick = function() {
+    document.body.classList.toggle('dark-mode');
+
+    if(document.body.classList.contains("dark-mode")){
+        darkMode.src = "img/light-mode.png";
+    }else{
+        darkMode.src = "img/dark_mode_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.png";
+    }
+ }
+
+
+
+
+
